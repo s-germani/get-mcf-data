4fc8ee8957e1d21b265d10fb34201de04ddd7dc4
s-germani <stefano.germani@unipg.it>
2022-10-22 12:43:36 +0200

    Aggiornamento opzione  E04

--- get_data.py
+++ get_data.py
@@ -14,7 +14,7 @@ def get_data(args):
 
         return
 
-    oudir = './'
+    outdir = './'
     doMove = False
     if len(args) == 2:
         outdir = args[1]
@@ -28,12 +28,12 @@ def get_data(args):
     #    print(f"Argument {i:>6}: {arg}")
 
     if args[0] == '--help':
-        print('get_data.py - scaria automaticamente i dati per le esrcitazioni del corso\n di Metodi Computazionali per la Fisica\n')
+        print('get_data.py - scarica automaticamente i dati per le esrcitazioni del corso\n di Metodi Computazionali per la Fisica\n')
         print('uso: python(3)  get_data.py opt [outdir]')
         print('opzioni disponibili:')
         print('  --help: mostra questo help')
         print('  --e03: eserctiazione E03 - Integrazione e Derivazione')
-        print('  --e04: eserctiazione E04 - Equazioni e Minimizzazione')
+        print('  --e04: eserctiazione E04 - Integrazione  + Minimizzazione')
 
         return
 
@@ -42,6 +42,16 @@ def get_data(args):
         
         remote_file1 = 'https://raw.githubusercontent.com/s-germani/metodi-computazionali-fisica/main/dati/integrazione_derivazione/vel_vs_time.csv'
         remote_file2 = 'https://raw.githubusercontent.com/s-germani/metodi-computazionali-fisica/main/dati/integrazione_derivazione/oscilloscope.csv'
+        
+        os.system('wget --directory-prefix {:}  {:}'.format(outdir, remote_file1))
+        os.system('wget --directory-prefix {:}  {:}'.format(outdir, remote_file2))
+            
+        return 
+
+    if args[0] == '--e04':
+        
+        remote_file1 = 'https://raw.githubusercontent.com/s-germani/metodi-computazionali-fisica/main/dati/integrazione_derivazione/vel_vs_time.csv'
+        remote_file2 = 'https://raw.githubusercontent.com/s-germani/metodi-computazionali-fisica/main/dati/equazioni_minimizzazione/fit_data.csv'
         
         os.system('wget --directory-prefix {:}  {:}'.format(outdir, remote_file1))
         os.system('wget --directory-prefix {:}  {:}'.format(outdir, remote_file2))
