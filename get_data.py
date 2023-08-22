0a5d1abf2ff7475292533f794ea78a191883de81
s-germani <stefano.germani@unipg.it>
2023-08-22 18:50:26 +0200

    Aggiornato per distinguere anni di corso

--- get_data.py
+++ get_data.py
@@ -1,112 +1,129 @@
 
 import sys,os
 import shutil
+import argparse
 
 
-def get_data(args):
+def parse_arguments():
 
 
-    if len(args) == 0 :
-        
-        print('\nE\' necessario fornire almeno una opzione!\n')
-        print('uso: python(3)  get_data.py opt [outdir]')
-        print('  per informazioni sulle opzioni disponibili: python(3) get_data.py --help')
+    parser = argparse.ArgumentParser(description='Scarica i dati per le esercitazioni del corso di Metodi Computazionali per la Fisica (UniPG).')
+    parser.add_argument('--year',          action="store",      required=True,  help='Anno di corso (2022,2023)')
+    parser.add_argument('--exn',           action="store",      required=True,  help='Numero Esercitazione')
+    parser.add_argument('--outdir',        action="store",      default='./',   help='Cartella di destinazione')
+    
+    return parser.parse_args()
 
-        return
 
-    outdir = './'
+
+def get_data():
+
+
+    args = parse_arguments()
+    
+
+    outdir = args.outdir 
     doMove = False
-    if len(args) == 2:
-        outdir = args[1]
+    if outdir != './':
         doMove = True
         print(' I file verranno copiati nella cartella', outdir)
 
     
+    ##################################### 2022 #####################################################à
+    if args.year == 2022:
         
-    #print(f"Arguments count: {len(args)}")
-    #for i, arg in enumerate(args):
-    #    print(f"Argument {i:>6}: {arg}")
+        if args.exn == 3:
+        
+            remote_file1 = 'https://raw.githubusercontent.com/s-germani/metodi-computazionali-fisica-2022/main/dati/integrazione_derivazione/vel_vs_time.csv'
+            remote_file2 = 'https://raw.githubusercontent.com/s-germani/metodi-computazionali-fisica-2022/main/dati/integrazione_derivazione/oscilloscope.csv'
+        
+            os.system('wget --directory-prefix {:}  {:}'.format(outdir, remote_file1))
+            os.system('wget --directory-prefix {:}  {:}'.format(outdir, remote_file2))
+            
+            return 
 
-    if args[0] == '--help':
-        print('get_data.py - scarica automaticamente i dati per le esrcitazioni del corso\n di Metodi Computazionali per la Fisica\n')
-        print('uso: python(3)  get_data.py opt [outdir]')
-        print('opzioni disponibili:')
-        print('  --help: mostra questo help')
-        print('  --e03: eserctiazione E03 - Integrazione e Derivazione')
-        print('  --e04: eserctiazione E04 - Integrazione  + Minimizzazione')
-        print('  --e06: eserctiazione E06 - Trasformate di Fourier')
-        print('  --e08: eserctiazione E08 - Monte Carlo Markov Chain')
-        print('  --e09: eserctiazione E09 - Moduli e Classi')
+        if args.exn == 4:
+        
+            remote_file1 = 'https://raw.githubusercontent.com/s-germani/metodi-computazionali-fisica-2022/main/dati/integrazione_derivazione/vel_vs_time.csv'
+            remote_file2 = 'https://raw.githubusercontent.com/s-germani/metodi-computazionali-fisica-2022/main/dati/equazioni_minimizzazione/fit_data.csv'
+        
+            os.system('wget --directory-prefix {:}  {:}'.format(outdir, remote_file1))
+            os.system('wget --directory-prefix {:}  {:}'.format(outdir, remote_file2))
+            
+            return 
 
-        return
+
+        if args.exn == 6:
+        
+            remote_file1 = 'https://raw.githubusercontent.com/s-germani/metodi-computazionali-fisica-2022/main/dati/trasformate_fourier/SN_m_tot_V2.0.csv'
+            remote_file2 = 'https://raw.githubusercontent.com/s-germani/metodi-computazionali-fisica-2022/main/dati/trasformate_fourier/data_sample1.csv'
+            remote_file3 = 'https://raw.githubusercontent.com/s-germani/metodi-computazionali-fisica-2022/main/dati/trasformate_fourier/data_sample2.csv'
+            remote_file4 = 'https://raw.githubusercontent.com/s-germani/metodi-computazionali-fisica-2022/main/dati/trasformate_fourier/data_sample3.csv'
+            remote_file5 = 'https://raw.githubusercontent.com/s-germani/metodi-computazionali-fisica-2022/main/dati/trasformate_fourier/copernicus_PG_selected.csv'
+
+        
+            os.system('wget --directory-prefix {:}  {:}'.format(outdir, remote_file1))
+            os.system('wget --directory-prefix {:}  {:}'.format(outdir, remote_file2))
+            os.system('wget --directory-prefix {:}  {:}'.format(outdir, remote_file3))
+            os.system('wget --directory-prefix {:}  {:}'.format(outdir, remote_file4))
+            os.system('wget --directory-prefix {:}  {:}'.format(outdir, remote_file5))
+
+            
+            return 
 
     
-    if args[0] == '--e03':
+        if args.exn == 8:
         
-        remote_file1 = 'https://raw.githubusercontent.com/s-germani/metodi-computazionali-fisica/main/dati/integrazione_derivazione/vel_vs_time.csv'
-        remote_file2 = 'https://raw.githubusercontent.com/s-germani/metodi-computazionali-fisica/main/dati/integrazione_derivazione/oscilloscope.csv'
+            remote_file1 = 'https://raw.githubusercontent.com/s-germani/metodi-computazionali-fisica-2022/main/dati/mcmc/absorption_line.csv'
         
-        os.system('wget --directory-prefix {:}  {:}'.format(outdir, remote_file1))
-        os.system('wget --directory-prefix {:}  {:}'.format(outdir, remote_file2))
-            
-        return 
+            os.system('wget --directory-prefix {:}  {:}'.format(outdir, remote_file1))
 
-    if args[0] == '--e04':
-        
-        remote_file1 = 'https://raw.githubusercontent.com/s-germani/metodi-computazionali-fisica/main/dati/integrazione_derivazione/vel_vs_time.csv'
-        remote_file2 = 'https://raw.githubusercontent.com/s-germani/metodi-computazionali-fisica/main/dati/equazioni_minimizzazione/fit_data.csv'
-        
-        os.system('wget --directory-prefix {:}  {:}'.format(outdir, remote_file1))
-        os.system('wget --directory-prefix {:}  {:}'.format(outdir, remote_file2))
-            
-        return 
+            return 
 
 
-    if args[0] == '--e06':
-        
-        remote_file1 = 'https://raw.githubusercontent.com/s-germani/metodi-computazionali-fisica/main/dati/trasformate_fourier/SN_m_tot_V2.0.csv'
-        remote_file2 = 'https://raw.githubusercontent.com/s-germani/metodi-computazionali-fisica/main/dati/trasformate_fourier/data_sample1.csv'
-        remote_file3 = 'https://raw.githubusercontent.com/s-germani/metodi-computazionali-fisica/main/dati/trasformate_fourier/data_sample2.csv'
-        remote_file4 = 'https://raw.githubusercontent.com/s-germani/metodi-computazionali-fisica/main/dati/trasformate_fourier/data_sample3.csv'
-        remote_file5 = 'https://raw.githubusercontent.com/s-germani/metodi-computazionali-fisica/main/dati/trasformate_fourier/copernicus_PG_selected.csv'
 
+        if args.exn == 9:
         
-        os.system('wget --directory-prefix {:}  {:}'.format(outdir, remote_file1))
-        os.system('wget --directory-prefix {:}  {:}'.format(outdir, remote_file2))
-        os.system('wget --directory-prefix {:}  {:}'.format(outdir, remote_file3))
-        os.system('wget --directory-prefix {:}  {:}'.format(outdir, remote_file4))
-        os.system('wget --directory-prefix {:}  {:}'.format(outdir, remote_file5))
+            remote_file1 = 'https://raw.githubusercontent.com/s-germani/metodi-computazionali-fisica-2022/main/dati/classi/hit_times_M0.csv'
+            remote_file2 = 'https://raw.githubusercontent.com/s-germani/metodi-computazionali-fisica-2022/main/dati/classi/hit_times_M1.csv'
+            remote_file3 = 'https://raw.githubusercontent.com/s-germani/metodi-computazionali-fisica-2022/main/dati/classi/hit_times_M2.csv'
+            remote_file4 = 'https://raw.githubusercontent.com/s-germani/metodi-computazionali-fisica-2022/main/dati/classi/hit_times_M3.csv'
+        
+            os.system('wget --directory-prefix {:}  {:}'.format(outdir, remote_file1))
+            os.system('wget --directory-prefix {:}  {:}'.format(outdir, remote_file2))
+            os.system('wget --directory-prefix {:}  {:}'.format(outdir, remote_file3))
+            os.system('wget --directory-prefix {:}  {:}'.format(outdir, remote_file4))
+
+            return
+
+
+
+        ##################################### 2023 #####################################################à
+        elif args.year == 2023:
 
             
-        return 
-
-    
-    if args[0] == '--e08':
+            if args.exn == 5:
         
-        remote_file1 = 'https://raw.githubusercontent.com/s-germani/metodi-computazionali-fisica/main/dati/mcmc/absorption_line.csv'
+                remote_file1 = 'https://raw.githubusercontent.com/s-germani/metodi-computazionali-fisica-2023/main/dati/classi/hit_times_M0.csv'
+                remote_file2 = 'https://raw.githubusercontent.com/s-germani/metodi-computazionali-fisica-2023/main/dati/classi/hit_times_M1.csv'
+                remote_file3 = 'https://raw.githubusercontent.com/s-germani/metodi-computazionali-fisica-2023/main/dati/classi/hit_times_M2.csv'
+                remote_file4 = 'https://raw.githubusercontent.com/s-germani/metodi-computazionali-fisica-2023/main/dati/classi/hit_times_M3.csv'
         
-        os.system('wget --directory-prefix {:}  {:}'.format(outdir, remote_file1))
+                os.system('wget --directory-prefix {:}  {:}'.format(outdir, remote_file1))
+                os.system('wget --directory-prefix {:}  {:}'.format(outdir, remote_file2))
+                os.system('wget --directory-prefix {:}  {:}'.format(outdir, remote_file3))
+                os.system('wget --directory-prefix {:}  {:}'.format(outdir, remote_file4))
 
-        return 
+                return
 
 
-
-    if args[0] == '--e09':
+            if args.exn == 6:
         
-        remote_file1 = 'https://raw.githubusercontent.com/s-germani/metodi-computazionali-fisica/main/dati/classi/hit_times_M0.csv'
-        remote_file2 = 'https://raw.githubusercontent.com/s-germani/metodi-computazionali-fisica/main/dati/classi/hit_times_M1.csv'
-        remote_file3 = 'https://raw.githubusercontent.com/s-germani/metodi-computazionali-fisica/main/dati/classi/hit_times_M2.csv'
-        remote_file4 = 'https://raw.githubusercontent.com/s-germani/metodi-computazionali-fisica/main/dati/classi/hit_times_M3.csv'
-        
-        os.system('wget --directory-prefix {:}  {:}'.format(outdir, remote_file1))
-        os.system('wget --directory-prefix {:}  {:}'.format(outdir, remote_file2))
-        os.system('wget --directory-prefix {:}  {:}'.format(outdir, remote_file3))
-        os.system('wget --directory-prefix {:}  {:}'.format(outdir, remote_file4))
-
-        return 
-
-
-
+                remote_file1 = 'https://raw.githubusercontent.com/s-germani/metodi-computazionali-fisica-2023/main/dati/integrazione_derivazione/vel_vs_time.csv'
+                
+                os.system('wget --directory-prefix {:}  {:}'.format(outdir, remote_file1))
+            
+            return 
 
 
 
@@ -114,4 +131,4 @@ def get_data(args):
 
 if __name__ == "__main__":
         
-    get_data(sys.argv[1:])
+    get_data()
